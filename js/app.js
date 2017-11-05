c81d66845bfd26b93d23cfbc0152f9aeb9d0ef78
Sushant Khanal <dreamerking114@gmail.com>
2017-11-05 12:54:36 +0545

    cat array added, and first element passed, with cat list appearing

--- js/app.js
+++ js/app.js
@@ -1,9 +1,47 @@
+ var initialCats = [
+        {
+            clickCount : 0,
+            name : 'Tabby',
+            imgSrc : 'img/434164568_fea0ad4013_z.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/bigtallguy/434164568',
+    		nicknames: ["kaley","valey","kuhura-chor"]
+
+        },
+        {
+            clickCount : 0,
+            name : 'Tiger',
+            imgSrc : 'img/4154543904_6e2428c421_z.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/xshamx/4154543904',
+            nicknames: ["bagh","thuloBiralo","jyanMara"]
+        },
+        {
+            clickCount : 0,
+            name : 'Scaredy',
+            imgSrc : 'img/22252709_010df3379e_z.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/kpjas/22252709',
+            nicknames: ["katar","xeruwa","maniMaruwa"]
+        },
+        {
+            clickCount : 0,
+            name : 'Shadow',
+            imgSrc : 'img/1413379559_412a540d29_z.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/malfet/1413379559',
+        	nicknames: ["chaya","parxai","sheetal"]
+        },
+        {
+            clickCount : 0,
+            name : 'Sleepy',
+            imgSrc : 'img/9648464288_2516b35537_z.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/onesharp/9648464288',
+            nicknames: ["alxi","nindralu","loddar"]
+        }
+    ]
+
 var Cat = function(data) {
 	this.clickCount = ko.observable(data.clickCount); 
 	this.name = ko.observable(data.name);
-	//this.title = ko.observable('infant');
 	this.imgSrc = ko.observable(data.imgSrc);
-	//this.imgAttribution = ko.observable('https:/www.flickr.com/photos/big...');
+	this.imgAttribution = ko.observable(data.imgAttribution);
 	this.nickNames = ko.observableArray(data.nicknames);
 	this.title = ko.computed(function(){
 		//debugger;
@@ -30,6 +68,15 @@ var Cat = function(data) {
 
 
 var ViewModel = function() {
+	var self = this;
+	this.catList = ko.observableArray();
+	initialCats.forEach(function(catItem) {
+		//debugger
+		self.catList.push(new Cat(catItem));
+		//debugger
+	});
+	this.currentCat = ko.observable(this.catList()[0]);
+/*
 	this.currentCat = ko.observable( new Cat({
 		clickCount: 0,
 		name: 'Tabby',
@@ -37,6 +84,7 @@ var ViewModel = function() {
 		imgAttribution: 'https:/www.flickr.com/photos/bigtallguy/434164568',
 		nicknames: ["kaley","valey","kuhura-chor"]
 	}) );
+*/	
 
 	this.incrementCounter = function() {
 		this.clickCount(this.clickCount() + 1);//since incrementCounter in in the currentCat binding context in html, this refers to currentCat()
